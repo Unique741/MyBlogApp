75e2df759f390689c285fced9c1d68cadb7405a4
Unique Pun <Uniquepun20@gmail.com>
2025-08-26 12:58:45 +0545

    Updated settings.py and commited

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Split by commas, e.g. "myblog-x-1.onrender.com,www.myblog.com"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
-
+# SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
+DEBUG = True
+SECRET_KEY = ['SECRET_KEY']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
